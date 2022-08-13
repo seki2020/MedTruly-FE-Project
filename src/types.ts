e06b9290df9639946358a18d0364bb6e466b4485
ximing <ximing@in-deco.com>
2022-08-13 13:53:21 +0800

    add ts types

---
+++ src/types.ts
@@ -0,0 +1,44 @@
+export enum APPOINTMENT_TYPE {
+  PENDING = "PENDING",
+  NEED_REVIEW = "NEED_REVIEW",
+  COMPLETED = "COMPLETED",
+}
+
+export enum SERVICE_TYPE {
+  typeA = "TYPE_A",
+  typeB = "TYPE_B",
+  typeC = "TYPE_C",
+}
+
+export interface AccountType {
+  firstName: string;
+  id: string;
+  lastName: string;
+}
+
+export interface PatientType {
+  account: AccountType;
+  id: string;
+}
+
+export interface SignessType {
+  account: AccountType;
+  id: string;
+}
+
+export interface AppointmentItemType {
+  id: string;
+  description: string;
+  serviceEnd: string;
+  serviceStart: string;
+  type: SERVICE_TYPE;
+  status: APPOINTMENT_TYPE;
+  patient: PatientType;
+  signee: SignessType;
+}
+
+export interface RawDataType {
+  task: AppointmentItemType[];
+  review: AppointmentItemType[];
+  done: AppointmentItemType[];
+}
